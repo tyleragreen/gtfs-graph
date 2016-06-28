76926eb0c403b53ec69f8f237272d80ceb59278c
Tyler Green <tgreen8091@gmail.com>
2016-06-28 13:32:47 +0000

    Add server test to ensure socket connection is created

--- test/serverSpec.js
+++ test/serverSpec.js
@@ -3,6 +3,13 @@
 var expect = require('chai').expect;
 var server = require('../lib/server.js');
 var http = require('http');
+var io = require('socket.io-client');
+
+var socketURL = 'http://gtfs-realtime-tgreen8091.c9users.io';
+var options = {
+  transports: ['websocket'],
+  'force new connection': true
+};
 
 describe('The server', function() {
   before(function () {
@@ -14,8 +21,16 @@ describe('The server', function() {
   });
 
   it('should return 200', function (done) {
-    http.get('http://gtfs-realtime-tgreen8091.c9users.io', function (res) {
+    http.get(socketURL, function (res) {
       expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+  
+  it('should allow a socket connection', function(done) {
+    var socket = io(socketURL, options);
+    socket.on('connect', function(){
+      expect(socket.connected).to.equal(true);
       done();
     });
   });
