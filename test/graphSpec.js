51a32c077d2d31c64f9e601d8ce2f70d9aaf4b11
Tyler Green <tgreen8091@gmail.com>
2016-07-17 23:26:40 +0000

    Checkin VERY BROKEN state during update of graph representation

--- test/graphSpec.js
+++ test/graphSpec.js
@@ -13,14 +13,14 @@ describe('The graph', function() {
   before(function() {
     let numNodes = 5;
     let edgeList = [
-        [ 1, 2], [ 2, 4], [ 1, 4]
+        [1,2], [2,4], [1,4]
       ];
     this.expectedGraph = [
-        [ 0, 0, 0, 0, 0 ],
-        [ 0, 0, 1, 0, 1 ],
-        [ 0, 0, 0, 0, 1 ],
-        [ 0, 0, 0, 0, 0 ],
-        [ 0, 0, 0, 0, 0 ],
+        [],
+        [0],
+        [0,1],
+        [0,0,0],
+        [0,1,1,0],
       ];
     this.graph = new Graph(edgeList, numNodes);
   })
@@ -58,4 +58,33 @@ describe('The graph', function() {
 });
 
 describe('A transit graph', function() {
+  before(function() {
+    var numNodes = 5;
+    var edgeList = [
+      { type: 'route', edge: [1,0] },
+      { type: 'transfer', edge: [2,1] },
+      { type: 'route', edge: [4,1] },
+      { type: 'route', edge: [3,2] },
+      { type: 'route', edge: [4,3] },
+      ];
+    this.expectedGraph = [
+      [],
+      [1],
+      [0,1],
+      [0,0,1],
+      [0,1,0,1]
+    ];
+    this.expectedSuperGraph = [
+      [],
+      [0],
+      [0,1],
+      [1,1,1],
+    ];
+    this.graph = new TransitGraph(edgeList, numNodes);
+  });
+  
+  it('works', function() {
+    expect(this.graph.getArray()).to.deep.equal(this.expectedGraph);
+    expect(this.graph.superGraph).to.deep.equal(this.expectedSuperGraph);
+  });
 });
