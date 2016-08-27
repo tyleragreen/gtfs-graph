c00a08e2e2d124edd17e2a54beae07c3b69dc418
Tyler Green <tgreen8091@gmail.com>
2016-08-27 20:11:42 +0000

    Move React Map and RouteList into separate modules

---
+++ public/js/route-list.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    var routes = this.props.stop.routes.map(function(route) {
+      return (
+        <Icon key={route} id={route.toLowerCase()} />
+      );
+    });
+    
+    return (
+      <span>{routes}</span>
+    );
+  }
+});
+
+var Icon = React.createClass({
+  render: function() {
+    var filename = 'icons/' + this.props.id + '.png';
+    return (
+      <img src={filename} />
+    );
+  }
+});
