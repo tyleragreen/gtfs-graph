17fb32275c22d82de229d2e9b01e3d3c5ec66be2
Tyler Green <tgreen8091@gmail.com>
2016-12-10 18:23:47 +0000

    Add ALL property to Enums and use Symbol.for() implementation for values to allow for symbol compares (MIGHT GET RID OF THIS) Remove print statements in genetic implementation

--- test/enumSpec.js
+++ test/enumSpec.js
@@ -6,8 +6,29 @@ var Enum = require('../lib/enum');
 
 var expect = require('chai').expect;
 
+let Mode = Enum(['BEST','WORST']);
+
 describe('An enum', function() {
-  it('can be created', function() {
-    let Modes = Enum(['BEST','WORST']);
+  it('has members that equal themselves', function() {
+    expect(Mode.BEST).to.equal(Symbol.for('BEST'));
+    expect(Mode.WORST).to.equal(Mode.WORST);
+  });
+  it('can return all its members', function() {
+    expect(Mode.ALL).to.deep.equal([Mode.BEST,Mode.WORST]); 
+  });
+  it('should fail to modify a member', function() {
+    expect(function() {
+      Mode.BEST = Symbol('medium best');
+    }).to.throw(Error);
+  });
+  it('should fail to add a member', function() {
+    expect(function() {
+      Mode.MEDIUM_BEST = Symbol('medium best');
+    }).to.throw(Error);
+  });
+  it('should fail to access a missing member', function() {
+    expect(function() {
+      console.log('Second worst value is: ' + Mode.SECOND_WORST);
+    }).to.throw(Error);
   });
 });
