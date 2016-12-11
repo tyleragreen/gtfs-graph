5db978bdff7f9e1ca9a2afeb825b9141c34bdf27
Tyler Green <tgreen8091@gmail.com>
2016-12-11 00:49:42 +0000

    Update API spec to use a getUrl method which returns a Promise

--- test/apiSpec.js
+++ test/apiSpec.js
@@ -9,18 +9,39 @@ var http = require('http');
 var SystemManager = require('../lib/server/systemManager');
 var System = require('../lib/server/system');
 
+//===================================================
+// API connection info
+//===================================================
+
+const host = (process.env.IP || '0.0.0.0');
+const port = (process.env.PORT || 3000);
+const pathname = '/api/v0';
+const API = `http://${host}:${port}${pathname}`;
+
 const testSys = {
   id: "test",
   location: "Testville",
   latitude: 40.5,
   longitude: -75.5
 };
-const httpOptions = {
-  host: (process.env.IP || '0.0.0.0'),
-  pathname: '/api/v0/system/test',
-  port: (process.env.PORT || 3000)
-};
 SystemManager.add(new System(testSys.id, testSys.location, testSys.latitude, testSys.longitude));
+
+function getUrl(url) {
+  return new Promise((resolve, reject) => {
+    let rawData = '';
+    
+    http.get(url, function(res) {
+      res.on('data', function(data) {
+        rawData += data;
+      });
+      res.on('end', function() {
+        resolve(JSON.parse(rawData));
+      });
+    }).on('error', e => {
+      reject(e); return;
+    });
+  });
+}
 
 describe('The API', function() {
   before(function() {
@@ -31,29 +52,13 @@ describe('The API', function() {
     server.close();
   });
   
-  it('should respond with a good status code', function(done) {
-    http.get(httpOptions, function(res) {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
-  });
-  it('should respond with a system', function(done) {
-    var host = (process.env.IP || '0.0.0.0');
-    var pathname = '/api/v0/system/test';
-    var port = (process.env.PORT || 3000);
-    let rawData = '';
-    http.get(`http://${host}:${port}${pathname}`, function(res) {
-      res.on('data', function(data) {
-        rawData += data;
-      });
-      res.on('end', function() {
-        let data = JSON.parse(rawData);
+  it('should respond with a system', function() {
+    return getUrl(`${API}/system/${testSys.id}`)
+      .then(function(data) {
         expect(data.id).to.equal(testSys.id);
         expect(data.location).to.equal(testSys.location);
         expect(data.latitude).to.equal(testSys.latitude);
         expect(data.longitude).to.equal(testSys.longitude);
-        done();
       });
-    });
   });
 });
