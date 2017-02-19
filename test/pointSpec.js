04c3c88c6431a55f0615e18fbd26ceafadc4c2ae
Tyler Green <tgreen8091@gmail.com>
2017-02-19 23:01:19 +0000

    Implement Haversine distance and add test

--- test/pointSpec.js
+++ test/pointSpec.js
@@ -11,4 +11,19 @@ describe('A point', function() {
     expect(point.latitude).to.equal(40);
     expect(point.longitude).to.equal(65);
   });
+  
+  it('can find the distance between two points', function() {
+    const nyc = new Point(40.7127, -74.0059);
+    const boston = new Point(42.358056, -71.063611);
+    const kmConversion = 1000;
+    const miConversion = 0.621371;
+    const distanceInKilometers = 306;
+    const distanceInMeters = distanceInKilometers * kmConversion;
+    const distanceInMiles = distanceInKilometers * miConversion;
+    const epsilon = 1;
+    
+    expect((nyc.distanceInKilometers(boston) - distanceInKilometers) < epsilon).to.equal(true);
+    expect((nyc.distanceInMeters(boston) - distanceInMeters) < epsilon).to.equal(true);
+    expect((nyc.distanceInMiles(boston) - distanceInMiles) < epsilon).to.equal(true);
+  });
 });
